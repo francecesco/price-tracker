000872456e5e1f916ec0afbd8a71906bcc98f2d1
fnegretti <f.negretti@advenias.it>
2026-06-06 19:47:37 +0200

    refactor: remove Keepa dependency, switch to direct Amazon scraping

--- src/config.py
+++ src/config.py
@@ -8,7 +8,6 @@ load_dotenv()
 class Config:
     telegram_token: str
     telegram_chat_id: int
-    keepa_api_key: str
     wishlist_url: str
     check_interval_hours: int
     report_day: str
@@ -19,7 +18,6 @@ def load_config() -> Config:
     return Config(
         telegram_token=os.environ["TELEGRAM_BOT_TOKEN"],
         telegram_chat_id=int(os.environ["TELEGRAM_CHAT_ID"]),
-        keepa_api_key=os.environ["KEEPA_API_KEY"],
         wishlist_url=os.environ["AMAZON_WISHLIST_URL"],
         check_interval_hours=int(os.environ.get("CHECK_INTERVAL_HOURS", "4")),
         report_day=os.environ.get("REPORT_DAY", "friday"),
